ad5c9cf8ca30f1b40215f0d77e008423fc9c1adf
Alexander <alexiceanroid@gmail.com>
2017-12-08 00:07:13 +0300

    ordering with the form

--- BethanysPieShop_2017_2/Migrations/20171207194317_Orders.Designer.cs
+++ BethanysPieShop_2017_2/Migrations/20171207194317_Orders.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BethanysPieShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171207194317_Orders")]
+    partial class Orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
